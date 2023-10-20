bffe54fa284c1028eb043dae66eb6c6aad803da4
arthurmorgan2 <nurakura9@gmail.com>
2023-10-20 20:20:57 +0700

    tes commit

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HeroSection = () => {
+    return (
+        <section>
+            <h1 className='text-white'>Portofolio Website</h1>
+            <p className='text-[#ADB7BE]'>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto voluptatem similique mollitia quo, blanditiis inventore soluta at fuga architecto nobis dolores, veniam atque cupiditate amet numquam reiciendis facilis perferendis itaque.
+            </p>
+        </section>
+    )
+}
+
+export default HeroSection
