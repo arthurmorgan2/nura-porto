ca63f5d101e0621ca4d80c6fcc95e39383a1da2a
arthurmorgan2 <nurakura9@gmail.com>
2023-10-29 16:51:13 +0700

    update mahor 29/10

--- src/components/ProjectsCard.jsx
+++ src/components/ProjectsCard.jsx
@@ -18,7 +18,7 @@ const ProjectsCard = ({ imgUrl, title, description, previewUrl, gitUrl }) => {
 
                 </div>
             </div>
-            <div className='text-white rounded-b-xl bg-[#181818] py-6 px-4'></div>
+            <div className='text-white rounded-b-xl bg-[#181818] py-6 px-4'>
                 <h5 className='text-xl font-semibold mb-2'>{title}</h5>
                 <p className='text-[#ADB7BE]'>{description}</p>
             </div>
